21010e2b5780d58884e2cb3a4038f94065f3d339
Karthik Dileep <karthikdileep003@gmail.com>
2022-09-29 18:10:34 +0530

    some problems are added

--- dsa/BasicProblems3.java
+++ dsa/BasicProblems3.java
@@ -19,7 +19,27 @@ public class BasicProblems3 {
         // factorsOfNum(num);
         // int num = sc.nextInt();
         // System.out.println(substractProductAndSum(num));
-        biggestNumUntilUserPressZero();
+        // biggestNumUntilUserPressZero();
+        // System.out.print("Enter the number to find the factorial: ");
+        // int num = sc.nextInt();
+        // factorialOfNum(num);
+        // System.out.print("Enter the hours: ");
+        // double hrs = sc.nextDouble();
+        // System.out.print("Enter the watt: ");
+        // double watt = sc.nextDouble();
+        // electricityBill(hrs, watt);
+        // System.out.print("Enter the limiting number to find avg: ");
+        // int n = sc.nextInt();
+        // avgOfNNumbers(n);
+        // System.out.print("Enter a string to reverse it: ");
+        // String str = sc.next();
+        // System.out.println(reverseString(str));
+        // System.out.print("Palindrome num :");
+        // int num = sc.nextInt();
+        // System.out.println(palindromeNum(num));
+        System.out.print("Enter an alphabet: ");
+        char input = sc.next().charAt(0);
+        vowelsOrConsonants(input);
         sc.close();
     }
     
@@ -80,9 +100,89 @@ public class BasicProblems3 {
         while (sc.nextInt() != 0) {
             largestNum = sc.nextInt();
         }
-        
+
         System.out.println("Largest number is: " + largestNum);
         sc.close();
     }
+    
+
+    //factorial of a number
+    static void factorialOfNum(int num) {
+        int factorial = 1;
+        for (int i = num; i >= 1; i--) {
+            factorial *= i;
+        }
+        System.out.println("Factorial of the number is :" + factorial);
+    }
+
+    //program to calculate monthly electricity bill
+    static void electricityBill(double hrs, double watt) {
+        double consumedEnergy = hrs * watt;
+        double kWh = consumedEnergy / 1000;
+        int electricityRateInIndia = 6;
+        double electricityBillPerDay = kWh * electricityRateInIndia;
+        System.out.println("Your per day electricity bill: " + electricityBillPerDay);
+        double monthlyElectricityBill = electricityBillPerDay * 30;
+        System.out.println("Electricity bill per month: " + monthlyElectricityBill);
+    }
+
+    //average of N numbers
+    static void avgOfNNumbers(int n) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += sc.nextInt();
+        }
+
+        double avg = sum / n;
+        System.out.println(avg);
+        sc.close();
+
+    }
+
+    //reverse a string
+    static String reverseString(String str) {
+        char[] arr = str.toCharArray();
+        String rev = "";
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            rev += arr[i];
+        }
+
+        return rev;
+    }
+
+    //palindrome number or not
+    static boolean palindromeNum(int num) {
+        int input = num;
+        int sum = 0;
+        int r = 0;
+
+        while (num > 0) {
+            r = num % 10;
+            sum = (sum * 10) + r;
+            num /= 10;
+        }
+
+        return input == sum;
+    }
+
+    //program to find vowels or consonants
+    static void vowelsOrConsonants(char input) {
+        switch (input) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.print("Is vowel");
+                break;
+            default:
+                System.out.print("Is consonant");
+        }
+    }
+
+
 
 }
